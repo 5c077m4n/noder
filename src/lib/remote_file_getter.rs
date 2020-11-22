b30a7468fc4c0338345e5b407ad027f41a16223e
Roee Shapira <roy.shap1@gmail.com>
2020-11-22 10:56:10 +0200

    Continued on remote data getters.

--- src/lib/remote_file_getter.rs
+++ src/lib/remote_file_getter.rs
@@ -1,13 +1,34 @@
 use reqwest;
+use serde::{Deserialize, Serialize};
+
 use crate::lib::consts::NODE_VERSION_INDEX_URL;
 
 lazy_static! {
     static ref HTTP_CLIENT: reqwest::Client = reqwest::Client::new();
 }
 
-pub async fn get_dist_index() -> reqwest::Result<()> {
-    let json_response = HTTP_CLIENT.get(NODE_VERSION_INDEX_URL).send().await?.json().await?;
-
-    Ok(())
+#[derive(Deserialize, Serialize)]
+pub struct VersionData {
+    pub date: String,
+    pub files: Vec<String>,
+    pub lts: bool,
+    pub modules: String,
+    pub npm: String,
+    pub openssl: String,
+    pub security: bool,
+    pub uv: String,
+    pub v8: String,
+    pub version: String,
+    pub zlib: String,
 }
-pub fn get_remote_file(version: &str) {}
+
+pub async fn get_dist_index() -> reqwest::Result<Vec<VersionData>> {
+    let json_response: Vec<VersionData> = HTTP_CLIENT.get(NODE_VERSION_INDEX_URL)
+        .send()
+        .await?
+        .json()
+        .await?;
+
+    Ok(json_response)
+}
+pub async fn get_remote_file(version: &str) {}
