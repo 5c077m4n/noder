bede1095d41d2db705a0f23ce9ac37522b1285b1
Roee Shapira <roy.shap1@gmail.com>
2020-11-23 08:56:10 +0200

    Added sha256 file name const.

--- src/lib/consts.rs
+++ src/lib/consts.rs
@@ -2,6 +2,7 @@ use std::{env::temp_dir, path::PathBuf};
 
 pub const NODE_VERSION_INDEX_URL: &str = "https://nodejs.org/dist/index.json";
 pub const NODE_DIST_URL: &str = "https://nodejs.org/dist/";
+pub const SUMCHECK_FILE: &str = "SHASUMS256.txt";
 
 pub const TMP_DIR_NAME: &str = ".noder";
 lazy_static! {
