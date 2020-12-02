c0ba3f7cd6e8ad22025527c69b72c0823f65834e
Roee Shapira <roy.shap1@gmail.com>
2020-12-02 14:49:53 +0200

    Minor performace improvement.

--- src/lib/utils/hash.rs
+++ src/lib/utils/hash.rs
@@ -8,14 +8,13 @@ use crate::lib::types::GeneralError;
 pub async fn read_sumcheck_to_map(sumcheck: &str) -> Result<HashMap<String, String>, GeneralError> {
     let mut hashmap: HashMap<String, String> = HashMap::new();
 
-    let sumcheck_lines_parts = sumcheck.lines().map(|line| {
-        line.split(' ')
+    sumcheck.lines().for_each(|line| {
+        let line_parts: Vec<&str> = line
+            .split(' ')
             .filter(|s| !s.is_empty())
-            .collect::<Vec<&str>>()
-    });
-    for line_parts in sumcheck_lines_parts {
+            .collect();
         hashmap.insert(line_parts[1].to_owned(), line_parts[0].to_owned());
-    }
+    });
 
     Ok(hashmap)
 }
