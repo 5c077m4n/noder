f30ac22ebe5a2a81412d2eb5c2c63c49f6d33f04
Roee Shapira <roy.shap1@gmail.com>
2020-12-02 14:51:42 +0200

    Minor lint.

--- src/lib/utils/hash.rs
+++ src/lib/utils/hash.rs
@@ -7,12 +7,8 @@ use crate::lib::types::GeneralError;
 
 pub async fn read_sumcheck_to_map(sumcheck: &str) -> Result<HashMap<String, String>, GeneralError> {
     let mut hashmap: HashMap<String, String> = HashMap::new();
-
     sumcheck.lines().for_each(|line| {
-        let line_parts: Vec<&str> = line
-            .split(' ')
-            .filter(|s| !s.is_empty())
-            .collect();
+        let line_parts: Vec<&str> = line.split(' ').filter(|s| !s.is_empty()).collect();
         hashmap.insert(line_parts[1].to_owned(), line_parts[0].to_owned());
     });
 
